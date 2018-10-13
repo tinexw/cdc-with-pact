90c9225a73da425e405a0337a421d8dd1d01e3de
tinexw <kristine.jetzke@kreuzwerker.de>
2018-10-13 12:31:27 +0200

    Add consumer branch jenkins job

--- infrastructure/jenkins/jenkins_home_init/jobs.groovy
+++ infrastructure/jenkins/jenkins_home_init/jobs.groovy
@@ -1,5 +1,6 @@
 def gitUrl = 'https://github.com/tinexw/cdc-with-pact'
 
+// Main build and deploy job for consumer and provider each (continuous deployment case)
 ['messaging-app', 'user-service'].each {
     def app = it
     pipelineJob("$app-build-and-deploy") {
@@ -20,6 +21,24 @@ def gitUrl = 'https://github.com/tinexw/cdc-with-pact'
     }
 }
 
+// Branch job for consumer
+pipelineJob("messaging-app-branch-with-removed-field") {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url(gitUrl)
+                    }
+                    branch('remove-field')
+                    extensions {}
+                }
+            }
+            scriptPath("messaging-app/jenkins/cd/Jenkinsfile")
+        }
+    }
+}
+// Provider job that only executes contract tests, usually triggered by webhook
 pipelineJob("user-service-run-contract-tests") {
     definition {
         cpsScm {
